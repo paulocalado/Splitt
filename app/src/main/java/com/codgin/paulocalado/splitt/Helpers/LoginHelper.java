b11658c5bf22e968caea847de1d7bcd414ae5a65
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2017-10-02 12:03:34 -0300

    Initial commit

---
+++ app/src/main/java/com/codgin/paulocalado/splitt/Helpers/LoginHelper.java
@@ -0,0 +1,26 @@
+package com.codgin.paulocalado.splitt.Helpers;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.codgin.paulocalado.splitt.LoginActivity;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+/**
+ * Created by paulocalado on 02/10/17.
+ */
+
+public class LoginHelper {
+
+    public static void loginSuccess(LoginButton loginButton, final Context context){
+
+        Profile profile = Profile.getCurrentProfile();
+        Toast.makeText(context, "Ola"+profile.getFirstName(), Toast.LENGTH_LONG).show();
+
+    }
+}
