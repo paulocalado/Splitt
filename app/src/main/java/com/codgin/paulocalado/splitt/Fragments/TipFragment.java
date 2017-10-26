6e2257937df6d6ddb112f69905f9f7ff8575624a
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2017-10-26 12:14:37 -0300

    Ajustes no layout de tip

--- app/src/main/java/com/codgin/paulocalado/splitt/Fragments/TipFragment.java
+++ app/src/main/java/com/codgin/paulocalado/splitt/Fragments/TipFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.codgin.paulocalado.splitt.Model.Table;
 import com.codgin.paulocalado.splitt.R;
 
 /**
@@ -27,6 +29,8 @@ public class TipFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Table table;
 
     private OnFragmentInteractionListener mListener;
 
@@ -65,7 +69,17 @@ public class TipFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tip, container, false);
+        View v = inflater.inflate(R.layout.fragment_tip, container, false);
+        TextView txtTotalMesaTip = (TextView)v.findViewById(R.id.txtTotalMesaTip);
+
+        Bundle bundleHome = this.getArguments();
+
+        table = (Table) bundleHome.getSerializable("table");
+
+        txtTotalMesaTip.setText(getContext().getResources().getString(R.string.text_total_mesa_sem_tip)+
+        String.format("%.2f", table.getTotal()));
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
