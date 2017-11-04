b62fce5d758cffa66f2659a0c6418567eac8ad69
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2017-11-03 21:11:02 -0300

    tentando fazer facebook funcionar

--- app/src/main/java/com/codgin/paulocalado/splitt/Activity/LoginActivity.java
+++ app/src/main/java/com/codgin/paulocalado/splitt/Activity/LoginActivity.java
@@ -116,7 +116,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RC_SIGN_IN) {
